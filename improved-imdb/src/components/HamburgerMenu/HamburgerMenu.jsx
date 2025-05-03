66213486826fa191099f396770ac3cc8f6d24f47
Jashandeep <132484906+jashan12002@users.noreply.github.com>
2025-05-03 21:20:21 +0530

    Refactor Navbar and HamburgerMenu components for improved mobile responsiveness

--- improved-imdb/src/components/HamburgerMenu/HamburgerMenu.jsx
+++ improved-imdb/src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -166,7 +166,7 @@ const HamburgerMenu = () => {
                     <path fill="none" d="M0 0h24v24H0V0z"></path>
                     <path d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zm0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1zM3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1z"></path>
                 </svg>
-                <span className='text-[15px] font-medium'>Menu</span>
+                <span className='text-[15px] font-medium md:block hidden'>Menu</span>
             </button>
 
             {isOpen && !isMobile && (
